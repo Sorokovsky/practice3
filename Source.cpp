08e9bce08b52ef47d2308efe0ba993676cbb6024
Sorokovsky <Sorokovskys@ukr.net>
2024-04-02 19:45:03 +0300

    form

--- Source.cpp
+++ Source.cpp
@@ -26,7 +26,80 @@ int main()
 		}
 		case 2:
 		{
+			cout << "\n \t Stadium types";
 			manager.showStadiumTypes();
+			break;
+		}
+		case 3:
+		{
+			Stadium stadium = enterStadium();
+			manager.add(stadium);
+			break;
+		}
+		case 4:
+		{
+			cout << "\n \t Stadiums";
+			manager.showStadiums();
+			break;
+		}
+		case 5:
+		{
+			Position position = enterPosition();
+			manager.add(position);
+			break;
+		}
+		case 6:
+		{
+			cout << "\n \t Positions";
+			manager.showPositions();
+			break;
+		}
+		case 7:
+		{
+			Coach coach = enterCoach();
+			manager.add(coach);
+			break;
+		}
+		case 8:
+		{
+			cout << "\n \t Coaches";
+			manager.showCoaches();
+			break;
+		}
+		case 9:
+		{
+			Team team = enterTeam();
+			manager.add(team);
+			break;
+		}
+		case 10:
+		{
+			cout << "\n \t Teams";
+			manager.showTeams();
+			break;
+		}
+		case 11:
+		{
+			Player player = enterPlayer();
+			manager.add(player);
+			break;
+		}
+		case 12:
+		{
+			cout << "\n \t Players";
+			manager.showPlayers();
+			break;
+		}
+		case 13:
+		{
+			Game game = enterGame();
+			manager.add(game);
+			break;
+		}
+		case 14:
+		{
+			cout << "\n \t Games";
+			manager.showGames();
 			break;
 		}
 		default:
